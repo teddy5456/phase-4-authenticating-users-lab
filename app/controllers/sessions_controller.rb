563cf78a5b08fb1164122c0ac691063ed4e6da93
teddy <teddywambua5456@gmail.com>
2023-07-03 01:37:11 +0300

    Completed assignment

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+
+    def destroy
+    session[:user_id] = nil
+    head :no_content
+  end
+  
+  def create
+    user = User.find_by(username: params[:username])
+    if user
+      session[:user_id] = user.id
+      render json: user
+    else
+      render json: { error: 'Invalid username' }, status: :unprocessable_entity
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :articles, only: [:index, :show]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Sessions routes
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
+  # Users routes
+  get '/me', to: 'users#show'
 end
